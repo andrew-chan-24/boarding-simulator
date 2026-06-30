6bbab6833dd8e1a1e8e62690d63a476a0d0418f6
Andrew Chan <andrew.chan24@imperial.ac.uk>
2026-06-30 22:40:33 +0800

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react            from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins : [react()],
-  base    : '/airplane-boarding/',   // ← must match your GitHub repo name exactly
+  base    : '/boarding-simulator/',   // ← must exactly match your repo name
 })
